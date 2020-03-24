4490372014eedad23971ad85ce1458140b069b88
Agathe Pinel <agp.com@mailbox.org>
2020-03-24 08:45:45 +0100

    ajout options pour onglet commandes

--- desktop/js/seniorcareconfortsecurity.js
+++ desktop/js/seniorcareconfortsecurity.js
@@ -354,10 +354,25 @@ function addCmdToTable(_cmd) {
     tr += '<span class="cmdAttr" data-l1key="id" style="display:none;"></span>';
     tr += '<input class="cmdAttr form-control input-sm" data-l1key="name" style="width : 140px;" placeholder="{{Nom}}">';
     tr += '</td>';
+
     tr += '<td>';
     tr += '<span class="type" type="' + init(_cmd.type) + '">' + jeedom.cmd.availableType() + '</span>';
     tr += '<span class="subType" subType="' + init(_cmd.subType) + '"></span>';
     tr += '</td>';
+
+    tr += '<td>';
+    tr += '<span><label class="checkbox-inline"><input type="checkbox" class="cmdAttr checkbox-inline" data-l1key="isVisible"/>{{Afficher}}</label></span> ';
+    tr += '<span><label class="checkbox-inline"><input type="checkbox" class="cmdAttr checkbox-inline" data-l1key="isHistorized"/>{{Historiser}}</label></span> ';
+//    tr += '<span><label class="checkbox-inline"><input type="checkbox" class="cmdAttr checkbox-inline" data-l1key="configuration data-l2key="timeline::enable"/>{{Suivre dans la timeline}}</label></span> ';
+    tr += '</td>';
+
+    tr += '<td>';
+    tr += '<input class="cmdAttr form-control input-sm" data-l1key="unite" placeholder="Unité" title="{{Unité}}">';
+    tr += '<input class="cmdAttr form-control input-sm" data-l1key="configuration" data-l2key="minValue" placeholder="{{Min}}" title="{{Min}}" style="margin-top : 5px;"> ';
+    tr += '<input class="cmdAttr form-control input-sm" data-l1key="configuration" data-l2key="maxValue" placeholder="{{Max}}" title="{{Max}}" style="margin-top : 5px;">';
+
+    tr += '</td>';
+
     tr += '<td>';
     if (is_numeric(_cmd.id)) {
         tr += '<a class="btn btn-default btn-xs cmdAction" data-action="configure"><i class="fa fa-cogs"></i></a> ';
--- desktop/php/seniorcareconfortsecurity.php
+++ desktop/php/seniorcareconfortsecurity.php
@@ -239,7 +239,11 @@ $eqLogics = eqLogic::byType($plugin->getId());
       <table id="table_cmd" class="table table-bordered table-condensed">
         <thead>
           <tr>
-            <th>{{Nom}}</th><th>{{Type}}</th><th>{{Action}}</th>
+            <th>{{Nom}}</th>
+            <th>{{Type}}</th>
+            <th>{{Paramètres}}</th>
+            <th>{{Option affichage}}</th>
+            <th>{{Action}}</th>
           </tr>
         </thead>
         <tbody>
